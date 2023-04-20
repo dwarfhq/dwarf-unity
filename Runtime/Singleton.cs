3040757779b91dfaf1618632323756a36ccf91e1
Flemming Westberg <mail@fwestberg.dk>
2023-04-20 13:58:38 +0200

    Add Dwarf namespace

--- Runtime/Singleton.cs
+++ Runtime/Singleton.cs
@@ -1,47 +1,50 @@
 using UnityEngine;
 
-/// <summary>
-/// Generic Singleton base class
-/// </summary>
-/// <typeparam name="T">Type of the class inheriting the Singleton behaviour</typeparam>
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+namespace Dwarf
 {
-    private static T _instance;
-
     /// <summary>
-    /// The Singleton instance
+    /// Generic Singleton base class
     /// </summary>
-    public static T Instance
+    /// <typeparam name="T">Type of the class inheriting the Singleton behaviour</typeparam>
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
-        get
+        private static T _instance;
+
+        /// <summary>
+        /// The Singleton instance
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    // If the instance is null, try to find an object of type T in the scene
+                    _instance = GameObject.FindObjectOfType<T>();
+
+                    if (_instance == null)
+                    {
+                        // If no object of type T can be found in the scene, create a new instance
+                        GameObject singleton = new GameObject(typeof(T).Name);
+                        _instance = singleton.AddComponent<T>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public virtual void Awake()
         {
             if (_instance == null)
             {
-                // If the instance is null, try to find an object of type T in the scene
-                _instance = GameObject.FindObjectOfType<T>();
-
-                if (_instance == null)
-                {
-                    // If no object of type T can be found in the scene, create a new instance
-                    GameObject singleton = new GameObject(typeof(T).Name);
-                    _instance = singleton.AddComponent<T>();
-                }
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
             }
-
-            return _instance;
-        }
-    }
-
-    public virtual void Awake()
-    {
-        if (_instance == null)
-        {
-            _instance = this as T;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
+            else
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
