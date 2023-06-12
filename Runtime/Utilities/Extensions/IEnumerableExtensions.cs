1288139702329d113f2a431afbc9610c2e68130f
Aleksander Fjellvang <afj@dwarf.dk>
2023-06-12 09:02:16 +0200

    added extension

---
+++ Runtime/Utilities/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dwarf.Utilities.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Shuffle an array in place using the Fisher-Yates algorithm
+        /// </summary>
+        /// <param name="array"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void Shuffle<T>(this IList<T> array)
+        {
+            // Loop through the array from the end to the beginning
+            for (int i = array.Count - 1; i > 0; i--)
+            {
+                // Generate a random index between 0 and i (inclusive)
+                int j = Random.Range(0, i + 1);
+
+                // Swap the values at indices i and j
+                (array[j], array[i]) = (array[i], array[j]);
+            }
+        }
+    }
+}
