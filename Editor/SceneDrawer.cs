61d8af091767fd8e1b76c2f24c73dfcf38a7d946
Aleksander <asf@dwarf.dk>
2026-09-09 09:35:54 +0200

    chore: syntax

--- Editor/SceneDrawer.cs
+++ Editor/SceneDrawer.cs
@@ -1,7 +1,7 @@
 ﻿#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
-using System.IO
+using System.IO;
 [CustomPropertyDrawer(typeof(SceneAttribute))]
 public class SceneDrawer : PropertyDrawer
 {
